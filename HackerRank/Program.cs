6521deb432350f3e8cbd62b83ad53e828ff87805
EDUARDO VILAR <e.vilar95@gmail.com>
2022-07-26 10:57:42 -0300

    Diagonal Difference

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,4 +1,6 @@
-﻿//using System.CodeDom.Compiler;
+﻿//Plus Minus
+
+//using System.CodeDom.Compiler;
 //using System.Collections.Generic;
 //using System.Collections;
 //using System.ComponentModel;
@@ -14,13 +16,6 @@
 
 //class Result
 //{
-
-//    /*
-//     * Complete the 'plusMinus' function below.
-//     *
-//     * The function accepts INTEGER_ARRAY arr as parameter.
-//     */
-
 //    public static void plusMinus(List<int> arr, int n)
 //    {
 
@@ -67,7 +62,9 @@
 
 //    }
 //}
+
 //=====================================================================================
+//Mini Max Sum
 
 //using System.CodeDom.Compiler;
 //using System.Collections.Generic;
@@ -130,43 +127,195 @@
 //}
 
 //=====================================================================================
+//Time Conversion
 
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Collections;
+//using System.ComponentModel;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.Serialization;
+//using System.Text.RegularExpressions;
+//using System.Text;
+//using System;
 
-class Result
-{
-    public static string timeConversion(string s)
-    {
+//class Result
+//{
+//    public static string timeConversion(string s)
+//    {
+//        bool sucesso = DateTime.TryParse(s, out DateTime time);
+//        if(sucesso)
+//        {
+//            return time.ToString("HH:mm:ss");
+//        }
+//        return "Success";
+//    }
 
-    }
+//}
 
-}
+//class Solution
+//{
+//    public static void Main(string[] args)
+//    {
+//        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+//        string s = Console.ReadLine();
 
-        string s = Console.ReadLine();
+//        string result = Result.timeConversion(s);
 
-        string result = Result.timeConversion(s);
+//        textWriter.WriteLine(result);
 
-        textWriter.WriteLine(result);
+//        textWriter.Flush();
+//        textWriter.Close();
+//    }
+//}
 
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}
+//=====================================================================================
+// Lonely integer
+
+//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Collections;
+//using System.ComponentModel;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.Serialization;
+//using System.Text.RegularExpressions;
+//using System.Text;
+//using System;
+
+//class Result
+//{
+//    public static int lonelyinteger(List<int> a)
+//    {
+//        var result = 0;
+//        foreach (int i in a)
+//        {
+//            result ^= i;
+
+//        }
+//        return result;
+
+
+
+//    }
+
+//}
+
+//class Solution
+//{
+//    public static void Main(string[] args)
+//    {
+//        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+//        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+//        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+//        int result = Result.lonelyinteger(a);
+
+//        textWriter.WriteLine(result);
+
+//        textWriter.Flush();
+//        textWriter.Close();
+//    }
+//}
+
+//=====================================================================================
+//Diagonal Difference
+
+//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Collections;
+//using System.ComponentModel;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.Serialization;
+//using System.Text.RegularExpressions;
+//using System.Text;
+//using System;
+
+//class Result
+//{
+//    public static int diagonalDifference(List<List<int>> arr)
+//    {
+//        int aux1 = 0;
+//        int aux2 = 0;
+//        int i = 0;
+//        int total = 0;
+//        int l = arr.Count - 1;
+//        List<int> diagonal1 = new();
+//        List<int> diagonal2 = new();
+//        do
+//        {
+//            if (arr[i][i] == arr[i][i])
+//            {
+//                diagonal1.Insert(i, arr[i][i]);
+//                aux1 = diagonal1[i] + aux1;
+//            }
+//            if (arr[i][l] == arr[i][l])
+//            {
+//                diagonal2.Insert(i, arr[i][l]);
+//                aux2 = diagonal2[i] + aux2;
+//            }
+//            i++;
+//            l--;
+
+//        } while (i < arr.Count);
+
+//        if(aux1 > aux2)
+//        {
+//            total = aux1 - aux2;
+//        }
+//        if (aux1 < aux2)
+//        {
+//            total = aux2 - aux1;
+//        }
+//        if (aux1 == aux2)
+//        {
+//            total = aux1 - aux2;
+//        }
+
+//        return total;
+//    }
+
+//}
+
+//class Solution
+//{
+//    public static void Main(string[] args)
+//    {
+//        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+//        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+//        List<List<int>> arr = new List<List<int>>();
+
+//        for (int i = 0; i < n; i++)
+//        {
+//            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+//        }
+
+//        int result = Result.diagonalDifference(arr);
+
+//        textWriter.WriteLine(result);
+
+//        textWriter.Flush();
+//        textWriter.Close();
+//    }
+//}
+
+//=====================================================================================
+//
+
+
